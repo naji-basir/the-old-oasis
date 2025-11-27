8b29dc9ffa28250761e816ec154014c370d0b815
Basir Naji <basirnaji007@gmail.com>
2025-11-27 15:28:14 +0430

    routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,73 @@
-import styled from "styled-components";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
-    <div>
-      <h1 className="bg-red-300">Naji</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="login" element={<Login />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="users" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
+// import styled from "styled-components";
+// import GlobalStyles from "./styles/GlobalStyle";
+// import Button from "./ui/Button";
+// import Input from "./ui/Input";
+// import Heading from "./ui/Heading";
+// import Row from "./ui/Row";
+
+// const StyledApp = styled.div`
+//   padding: 20px;
+// `;
+
+// function App() {
+//   return (
+//     <>
+//       <GlobalStyles />
+//       <StyledApp>
+//         <Row>
+//           <Row type="horizontal">
+//             <Heading as="h1">Naji</Heading>
+//             <div>
+//               <Heading as="h2">Check in & Check out</Heading>
+//               <Button>Check In</Button>
+//               <Button variation="secondary" size="small">
+//                 Check Out
+//               </Button>
+//             </div>
+//           </Row>
+//           <Row>
+//             <Heading as="h3">Fom</Heading>
+//             <Input type="number" placeholder="Number of guests"></Input>
+//             <Input type="number" placeholder="Number of guests"></Input>
+//           </Row>
+//         </Row>
+//       </StyledApp>
+//     </>
+//   );
+// }
+
+// export default App;
